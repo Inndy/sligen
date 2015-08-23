053fdd9337df523b73f67660240dde8b2554d150
m80126colin <m80126colin@gmail.com>
2015-08-23 10:58:11 +0800

    Use new buzzword

--- src/wordgen/wordgen.py
+++ src/wordgen/wordgen.py
@@ -4,7 +4,7 @@ class Wordgen(object):
     def __init__(self):
         self.verb  = []
         self.other = []
-        f = open('data/buzzword.txt', 'r')
+        f = open('data/newbuzzword.txt', 'r')
         for line in f.readlines():
             dat   = line.split('\t')
             word  = dat[0]
