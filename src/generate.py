aa83e4a519461f5e5f9f508163579bb6429c1ddb
Zekt <asfgjk123123@gmail.com>
2015-08-23 10:12:43 +0800

    Just a fxxking commit

--- src/generate.py
+++ src/generate.py
@@ -1,20 +1,35 @@
+import os
+import sys
 from slidegen import Generator
 from slidegen import DataProvider
 
+if len(sys.argv) < 2:
+    print('Usage: %s output-path' % sys.argv[0])
+    exit(1)
+
+class GeneratorBridge(object):
+    def __init__(self, generator, output_path):
+        self.generator = generator
+        self.path = output_path
+        self.index = 0
+
+    def __hook(self, val):
+        self.index += 1
+        with open(os.path.join(self.path, '%.3d.md' % self.index), 'w') as fo:
+            fo.write(val)
+        return val
+
+    def __getattr__(self, name):
+        f = getattr(self.generator, name)
+        def _proc(*args):
+            return self.__hook(f(*args))
+        return _proc
+
 g = Generator(DataProvider())
+g = GeneratorBridge(g, sys.argv[1])
 
-print('''
-title: Pitching like a boss
-controls: false
-output: slide.html
-
----
-''')
-print(g.cover())
-print(g.content())
+g.cover()
+g.content()
 for i in range(4):
-    print(g.content())
-print(g.full_image())
-print('''
-# Q & A
-''')
+    g.content()
+g.full_image()
